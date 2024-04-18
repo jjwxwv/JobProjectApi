d90262c7bf5fcefe63fe4853987c940314bc2bb9
jj <jjwxwv@hotmail.com>
2024-04-18 13:45:46 +0700

    edit table name

--- src/entities/Category.ts
+++ src/entities/Category.ts
@@ -2,7 +2,7 @@ import { Entity, OneToMany } from "typeorm";
 import { Common } from "./utils/Common";
 import { Post } from "./Post";
 
-@Entity("categories")
+@Entity("category")
 export class Category extends Common {
   @OneToMany(() => Post, (post) => post.category)
   posts: Post[];
